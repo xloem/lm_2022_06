d1438c73f8b73beb5588e278dd54e083a1c38888
John Doe <johndoe@example.com>
2022-06-20 21:47:08 -0400

    hack for device

--- src/model.py
+++ src/model.py
@@ -42,7 +42,7 @@ class RWKV_ChannelMix(nn.Module):
         self.value = nn.Linear(hidden_sz, n_embd, bias=False)
 
     def time_shift(self, x):
-        return torch.cat([self.xx[...,None,:], x], dim=-2)[...,:-1,:]
+        return torch.cat([self.xx.to(x.device)[...,None,:], x], dim=-2)[...,:-1,:]
 
     def forward(self, x):
         xx = x[...,-1,:]
@@ -73,7 +73,7 @@ class RWKV_TimeMix(nn.Module):
         self.output = nn.Linear(n_embd, n_embd, bias=False)
 
     def time_shift(self, x):
-        return torch.cat([self.xx[...,None,:], x], dim=-2)[...,:-1,:]
+        return torch.cat([self.xx.to(x.device)[...,None,:], x], dim=-2)[...,:-1,:]
 
     def forward(self, x):
         B, T, C = x.size()
